d55d3411ac05f963c89d602387a92d90918f9928
unknown <ag.macazz@gmail.com>
2023-06-22 13:58:42 -0300

    yup

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,45 @@
 import { useForm } from "react-hook-form";
 import { useState } from "react";
+import { yupResolver } from "@hookform/resolvers/yup";
 import ShowData from "./components/ShowData";
+import * as yup from "yup";
 import "./App.css";
+
+const schema = yup
+  .object({
+    firstName: yup.string().required(),
+    lastName: yup.string().required(),
+    age: yup.number().positive().integer().required(),
+    address: yup.string().required(),
+  })
+  .required();
 
 export default function App() {
   const [arrayOfUsers, setArrayOfUsers] = useState([]);
+  const [actualUser, setActualUser] = useState({});
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
   const onSubmit = (data) => {
     const id = Math.floor(Math.random() * 100000);
     const newData = { ...data, id: id };
     console.log(newData);
     setArrayOfUsers([...arrayOfUsers, newData]);
+    document.getElementById("form").reset(); // reset form
+    setActualUser({});
   };
-console.log(errors)
+  ////
+  console.log(errors);
   return (
-    <form className="container d flex row" onSubmit={handleSubmit(onSubmit)}>
+    <form
+      id="form"
+      className="container d flex row"
+      onSubmit={handleSubmit(onSubmit)}
+    >
       {arrayOfUsers
         ? arrayOfUsers.map((m) => (
             <ShowData
@@ -28,30 +49,18 @@ console.log(errors)
               lastName={m.lastName}
               age={m.age}
               address={m.address}
+              setActualUser={setActualUser}
             />
           ))
         : ""}
-      <input
-        className="m-1"
-        {...register("firstName", {
-          required: true,
-          maxLength: 20,
-        })}
-      />
-      <p>{errors.firstName?.type}</p>
-      <input
-        className="m-1"
-        {...register("lastName", { required: true, pattern: /^[A-Za-z]+$/i })}
-      />
-      <input
-        className="m-1"
-        type="number"
-        {...register("age", { required: true, min: 18, max: 99 })}
-      />
-      <input
-        className="m-1"
-        {...register("address", { required: true, maxLength: 20 })}
-      />
+      <input className="m-1" {...register("firstName")} />
+      <p>{errors.firstName?.message}</p>
+      <input className="m-1" {...register("lastName")} />
+      <p>{errors.lastName?.message}</p>
+      <input className="m-1" {...register("age")} />
+      <p>{errors.age?"age invalid":""}</p>
+      <input className="m-1" {...register("address")} />
+      <p>{errors.address?.message}</p>
       <input className="m-1 btn btn-success" type="submit" />
     </form>
   );
--- src/components/ShowData.jsx
+++ src/components/ShowData.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 
-const ShowData = ({ firstName, lastName, age, address, id }) => {
+const ShowData = ({ firstName, lastName, age, address, id, setActualUser }) => {
+  const selectUserFromArray = () => {
+    setActualUser({
+      firstName: firstName,
+      lastName: lastName,
+      age: age,
+      address: address,
+    });
+  };
   return (
-    <div className="container border m-1">
+    <div className="container border m-1" onClick={selectUserFromArray}>
       <div>{id}</div>
       <div>{firstName}</div>
       <div>{lastName}</div>
